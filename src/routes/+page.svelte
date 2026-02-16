65303e6952dbebe227cde07a07edd6174bd14d94
Zaryab Ahmed <za366@drexel.edu>
2026-02-16 00:36:04 -0500

    add header component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 import { Heading } from "flowbite-svelte";
+import Header from "./Header.svelte";
 import PointsOverview from "./PointsOverview.svelte";
 
 let user = $state({ fullName: "Juan Doe" });
 </script>
+
+<Header />
 
 <main class="max-w-[1280px] mx-auto">
     <div id="greeting" class="m-4">
---
+++ src/routes/Header.svelte
@@ -0,0 +1,5 @@
+<nav class="sticky top-0 p-2.5 px-16 bg-white border-b-1 border-gray flex flex-row justify-between shadow-sm">
+    <a href="/" class="font-bold text-5xl font-serif text-primary-800">
+        GSA+
+    </a>
+</nav>
