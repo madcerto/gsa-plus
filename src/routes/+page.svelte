d1cdc2fb78eef9d1891fe4f2b8ca8fb3b90f8cc1
Zaryab Ahmed <za366@drexel.edu>
2026-02-16 00:50:00 -0500

    show lists of registered and upcoming events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-import { Heading } from "flowbite-svelte";
+import { Heading, Hr } from "flowbite-svelte";
 import Header from "./Header.svelte";
 import PointsOverview from "./PointsOverview.svelte";
+import EventList from "./EventList.svelte";
 
 let user = $state({ fullName: "Juan Doe", stats: { points: 42, hours: 7 }, avatarImgUrl: "https://flowbite-svelte.com/images/profile-picture-3.webp" });
+let registeredEventIds = $state([0, 1, 2, 3, 4]);
+let upcomingEventIds = $state([5, 6, 7, 8, 9, 10, 11]);
 </script>
 
 <Header {user} />
@@ -15,4 +18,14 @@ let user = $state({ fullName: "Juan Doe", stats: { points: 42, hours: 7 }, avata
     </div>
 
     <PointsOverview points={user.stats.points} hours={user.stats.hours} />
+
+    <Hr />
+
+    <Heading tag="h2" class="m-4">Your events:</Heading>
+    <EventList eventIds={registeredEventIds} imgUrl="https://flowbite-svelte.com/images/image-1.webp" />
+
+    <Hr />
+
+    <Heading tag="h2" class="m-4">Explore upcoming events:</Heading>
+    <EventList eventIds={upcomingEventIds} imgUrl="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" />
 </main>
---
+++ src/routes/EventList.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { Card, Heading } from "flowbite-svelte";
+
+const { eventIds, imgUrl } = $props();
+</script>
+
+<div class="flex flex-row flex-wrap">
+{#each eventIds as eventId}
+    <Card href="/events/{eventId}" img={eventId % 2 == 0 ? imgUrl : undefined} horizontal class="m-4" imgClass="max-w-40">
+    <div class="m-4 flex flex-col gap-2">
+        <Heading tag="h4">Event title</Heading>
+        <p>A quick description of what will happen at this event</p>
+        <span class="text-secondary-900 mt-auto">Mar. 1st | 10am - 3pm</span>
+    </div>
+    </Card>
+{/each}
+</div>
