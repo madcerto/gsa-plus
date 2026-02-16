363733fd841df22f4ce719775341296c648faf25
Zaryab Ahmed <za366@drexel.edu>
2026-02-16 00:53:28 -0500

    increase horizontal padding on header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,7 +4,7 @@ import { Avatar, NavHamburger, Dropdown, DropdownHeader, DropdownGroup, Dropdown
 const { user } = $props();
 </script>
 
-<nav class="sticky top-0 p-2.5 px-16 bg-white border-b-1 border-gray flex flex-row justify-between shadow-sm">
+<nav class="sticky top-0 p-2.5 px-24 bg-white border-b-1 border-gray flex flex-row justify-between shadow-sm">
     <a href="/" class="font-bold text-5xl font-serif text-primary-800">
         GSA+
     </a>
