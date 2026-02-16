112770137e04ab586de99b61c2f279b92b9fdfa5
Zaryab Ahmed <za366@drexel.edu>
2026-02-16 00:26:34 -0500

    initial project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { Heading } from "flowbite-svelte";
+
+let user = $state({ fullName: "Juan Doe" });
+</script>
+
+<main class="max-w-[1280px] mx-auto">
+    <div id="greeting" class="m-4">
+        <Heading tag="h2">Welcome back, {user.fullName}</Heading>
+        <Heading tag="h4" class="font-normal">Time to get involved in GSA!</Heading>
+    </div>
+</main>
