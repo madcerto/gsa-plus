e297bd02432cee50b3201e6f3bf34d7abcacbf66
Zaryab Ahmed <za366@drexel.edu>
2026-02-16 02:07:14 -0500

    show location after date and time in event details

--- src/routes/event/[id]/+page.svelte
+++ src/routes/event/[id]/+page.svelte
@@ -14,8 +14,8 @@ const event = $state({ title: "Event title", description: "A quick description o
                 {event.description}
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <Card class="p-4 text-secondary-900 flex flex-row gap-4"><MapPinAltOutline class="shrink-0 h-6 w-6" /> {event.location}</Card>
             <Card class="p-4 text-secondary-900 flex flex-row gap-4"><CalendarMonthOutline class="shrink-0 h-6 w-6" /> {event.date} | {event.time}</Card>
+            <Card class="p-4 text-secondary-900 flex flex-row gap-4"><MapPinAltOutline class="shrink-0 h-6 w-6" /> {event.location}</Card>
         </div>
     </div>
 </main>
